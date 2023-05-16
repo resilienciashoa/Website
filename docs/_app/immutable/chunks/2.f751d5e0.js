555dacc1e304444031efd16d21d1892d773618d8
itzjakm <itzjak.moss@gmail.com>
2023-05-15 21:25:39 -0600

    .

---
+++ docs/_app/immutable/chunks/2.f751d5e0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.bff100ae.js";export{t as component};
---
+++ docs/_app/immutable/chunks/4.629919ee.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_materia_-_specificProyecto_-page.svelte.adfcd99d.js";export{t as component};
